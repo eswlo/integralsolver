c51ce09e00d6a6ba0ef554fb0c8792194f3e9276
eswlo <sl3923@columbia.edu>
2024-04-09 15:08:30 -0700

    added trig evaluation and tests

--- evaluate.pl
+++ evaluate.pl
@@ -1,7 +1,6 @@
 :- module(evalutate, [eval/4, exp_calc/3]).
 %% ============================ Evaluator ===================================
 % eval(F, D, X, V) is true if V is the value by plugging X into variable D in F
-% for trig, only sin and cos are considered at this point.
 eval(K, _, _, K) :-
     number(K).
 eval(D, D, X, X) :-
@@ -27,61 +26,180 @@ eval(D^N, D, X, V) :-
     exp_calc(X, N, V).
 
 
+
+eval(sin(D), D, X, 0) :-
+    X mod 180 =:= 0,
+    !.
+eval(_*sin(D), D, X, 0) :-
+    X mod 180 =:= 0,
+    !.
+eval(cos(D), D, X, 0) :-
+    X mod 360 =:= 90,
+    !.
+eval(cos(D), D, X, 0) :-
+    X mod 360 =:= 270,
+    !.
+eval(_*cos(D), D, X, 0) :-
+    X mod 360 =:= 90,
+    !.
+eval(_*cos(D), D, X, 0) :-
+    X mod 360 =:= 270,
+    !.
+eval(tan(D), D, X, 0) :-
+    X mod 180 =:= 0,
+    !.
+eval(_*tan(D), D, X, 0) :-
+    X mod 180 =:= 0,
+    !.
+
+
+
 eval(sin(D), D, X, V) :-
     R is (X * pi / 180),
     V is sin(R).
-
 eval(-sin(D), D, X, -V) :-
     R is (X * pi / 180),
     V is sin(R).
-
-eval(A*sin(D), D, X, EA* V) :-
+eval(A*sin(D), D, X, V) :-
     eval(A, D, X, EA),
     R is (X * pi / 180),
-    V is sin(R).
-
+    S is sin(R),
+    V is EA*S.
 eval(cos(D), D, X, V) :-
     R is (X * pi / 180),
     V is cos(R).
-
 eval(-cos(D), D, X, -V) :-
     R is (X * pi / 180),
     V is cos(R).    
-
-eval(A*cos(D), D, X, EA* V) :-
+eval(A*cos(D), D, X, V) :-
     eval(A, D, X, EA),
     R is (X * pi / 180),
-    V is cos(R).
+    C is cos(R),
+    V is EA*C.
+eval(tan(D), D, X, V) :-
+    R is (X * pi / 180),
+    S = sin(R),
+    C = cos(R),
+    dif(C, 0),
+    V is S/C.
+eval(-tan(D), D, X, -V) :-
+    R is (X * pi / 180),
+    S = sin(R),
+    C = cos(R),
+    dif(C, 0),
+    V is S/C. 
+eval(A*tan(D), D, X, V) :-
+    eval(A, D, X, EA),
+    R is (X * pi / 180),
+    S = sin(R),
+    C = cos(R),
+    dif(C, 0),
+    T is S/C,
+    V is EA*T.
+eval(csc(D), D, X, _) :-
+    eval(sin(D), D, X, S),
+    S = 0,
+    !,
+    fail.
+eval(csc(D), D, X, V) :-
+    eval(sin(D), D, X, S),
+    V is 1/S.
+eval(-csc(D), D, X, _) :-
+    eval(sin(D), D, X, S),
+    S = 0,
+    !,
+    fail.
+eval(-csc(D), D, X, -V) :-
+    eval(sin(D), D, X, S),
+    V is 1/S.
+eval(_*csc(D), D, X, _) :-
+    eval(sin(D), D, X, S),
+    S = 0,
+    !,
+    fail.
+eval(A*csc(D), D, X, V) :-
+    eval(A, D, X, EA),
+    eval(sin(D), D, X, S),
+    C is 1/S,
+    V is EA*C.
+eval(sec(D), D, X, _) :-
+    eval(cos(D), D, X, S),
+    S = 0,
+    !,
+    fail.
+eval(sec(D), D, X, V) :-
+    eval(cos(D), D, X, S),
+    V is 1/S.
+eval(-sec(D), D, X, _) :-
+    eval(cos(D), D, X, S),
+    S = 0,
+    !,
+    fail.
+eval(-sec(D), D, X, -V) :-
+    eval(cos(D), D, X, S),
+    V is 1/S.
+eval(_*sec(D), D, X, _) :-
+    eval(cos(D), D, X, S),
+    S = 0,
+    !,
+    fail.
+eval(A*sec(D), D, X, V) :-
+    eval(A, D, X, EA),
+    eval(cos(D), D, X, S),
+    C is 1/S,
+    V is EA*C.
+
+
+eval(cot(D), D, X, _) :-
+    eval(tan(D), D, X, S),
+    S = 0,
+    !,
+    fail.
+eval(cot(D), D, X, V) :-
+    eval(tan(D), D, X, S),
+    V is 1/S.
+eval(-cot(D), D, X, _) :-
+    eval(tan(D), D, X, S),
+    S = 0,
+    !,
+    fail.
+eval(-cot(D), D, X, -V) :-
+    eval(tan(D), D, X, S),
+    V is 1/S.
+eval(_*cot(D), D, X, _) :-
+    eval(tan(D), D, X, S),
+    S = 0,
+    !,
+    fail.
+eval(A*cot(D), D, X, V) :-
+    eval(A, D, X, EA),
+    eval(tan(D), D, X, S),
+    C is 1/S,
+    V is EA*C.
+
+
 
 
 eval(log(e, abs(D)), D, X, V) :-
     X >= 0,
     V is log(X) / log(e).
-
 eval(log(e, abs(D)), D, X, V) :-
     X < 0,
     V is log(-X) / log(e).
-
 eval(log(B, A), _, _, V) :-
     number(B),
     number(A),
     B > 0,
     A >= 0, 
     V is log(A) / log(B).
-
 eval(log(e, A), _, _, V) :-
     number(A),
     A >= 0, 
     V is log(A).
-
 eval(log(B, D), D, X, V) :-
     B > 0,
     X >=0, 
     V is log(X) / log(B).
-    
-
-
-
     
 
 
